1e26b58120214e588bafa2d80638aa789c192441
Tenzile04 <tenzileabdiyeva2004@gmail.com>
2023-11-29 13:25:41 +0400

    first commit

---
+++ AllUpTask/Migrations/20231129073344_CreateImagesTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AllUpTask.Migrations
+{
+    public partial class CreateImagesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductImage",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    IsPoster = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductImage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProductImage_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImage",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductImage");
+        }
+    }
+}
---
+++ AllUpTask/Migrations/20231129082612_CreateTable.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AllUpTask.Migrations
+{
+    public partial class CreateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImage_Products_ProductId",
+                table: "ProductImage");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductImage",
+                table: "ProductImage");
+
+            migrationBuilder.RenameTable(
+                name: "ProductImage",
+                newName: "ProductImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductImage_ProductId",
+                table: "ProductImages",
+                newName: "IX_ProductImages_ProductId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "ProductImages",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductImages",
+                table: "ProductImages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImages_Products_ProductId",
+                table: "ProductImages",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductImages_Products_ProductId",
+                table: "ProductImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ProductImages",
+                table: "ProductImages");
+
+            migrationBuilder.RenameTable(
+                name: "ProductImages",
+                newName: "ProductImage");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ProductImages_ProductId",
+                table: "ProductImage",
+                newName: "IX_ProductImage_ProductId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "ProductImage",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ProductImage",
+                table: "ProductImage",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductImage_Products_ProductId",
+                table: "ProductImage",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
